19ed29c0db8c12b2654139824a7f5888058f5a14
jcant <jcant.gm@gmail.com>
2017-12-27 09:33:13 +0200

    First HelloWorld Commit

---
+++ HelloWorld/src/com/gmail/gm/jcant/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.gm.jcant;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world from jCant!");
+
+	}
+
+}
