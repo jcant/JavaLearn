6b0918e08ad6504b50ff5912b6c202e57af567d7
jcant <jcant.gm@gmail.com>
2017-12-27 11:21:41 +0200

    some test commit

--- HelloWorld/src/com/gmail/gm/jcant/Main.java
+++ HelloWorld/src/com/gmail/gm/jcant/Main.java
@@ -5,9 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world from jCant!");
-		System.out.println("Goodbye World...");
-		System.out.println("I am off...");
+
 
 	}
 
