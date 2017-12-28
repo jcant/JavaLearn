afc1c6495b9d113394d580d8137462e07123b835
jcant <jcant.gm@gmail.com>
2017-12-28 14:57:38 +0200

    add HomeWork22

---
+++ HomeWork22/src/com/gmail/gm/jcant/Main.java
@@ -0,0 +1,24 @@
+package com.gmail.gm.jcant;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// !!! The sides of the triangle must correspond the requirements:
+		// - maximum side must be less than sum of two others
+		// If not so the triangle does not exist
+		
+		double a = 24.5;
+		double b = 48.89999;
+		double c = 24.4;
+		
+		double p = (a + b + c)/2.0;
+		
+		double square = Math.sqrt(p*(p-a)*(p-b)*(p-c));
+		
+		System.out.println("Triangle square = "+square);
+		
+
+	}
+
+}
