02d562f0bf9f9e5caa9d99d4b96aa270565f9e5b
jcant <jcant.gm@gmail.com>
2017-12-31 02:58:07 +0200

    HW42L2 - finding simple numbers for 100 (back to the task conditions:( )

--- HomeWork42L2/src/com/gmail/gm/jcant/Main.java
+++ HomeWork42L2/src/com/gmail/gm/jcant/Main.java
@@ -4,15 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		long range = 30000000L;
-		long count=0;
+		int range = 100;
 		
-		for (long i=2;i<=range; i++) {
+		for (int i=2;i<=range; i++) {
 			
-			long currentRange = (long)Math.sqrt(i);
+			int currentRange = (int)Math.sqrt(i);
 			boolean simpleFlag = true;
 			
-			for(long j=2; j<=currentRange; j++) {
+			for(int j=2; j<=currentRange; j++) {
 				if (i%j == 0) {
 					simpleFlag = false;
 					break;
@@ -21,16 +20,12 @@ public class Main {
 			
 			if (simpleFlag) {
 				System.out.print(""+i+"; ");
-				count++;
 			}
 			
 			if (i%100==0) {
 				System.out.println();
 			}
 		}
-		
-		System.out.println();
-		System.out.println("Total: "+count);
 
 	}
 
