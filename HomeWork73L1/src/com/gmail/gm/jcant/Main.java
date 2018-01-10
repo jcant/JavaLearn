625c7b77de90e67120ff5c754d63c2ad2c0c4d52
jcant <jcant.gm@gmail.com>
2018-01-10 15:06:41 +0200

    HW73L1 - binary to decimal (change int to long)

--- HomeWork73L1/src/com/gmail/gm/jcant/Main.java
+++ HomeWork73L1/src/com/gmail/gm/jcant/Main.java
@@ -9,45 +9,49 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		String binNumber;
 
-		System.out.println("Please input binary number: ");
+		System.out.println("Please input binary number (max - 63 chars): ");
 		binNumber = sc.nextLine();
 		sc.close();
 
-		int result = binToDecimal(stringToBinary(binNumber));
+		if (binNumber.length() > 63) {
 
-		System.out.println("Decimal: " + result);
-		
-		int x = (int)1000b0;
+			System.out.println("You entered too big number!");
+
+		} else {
+
+			System.out.println("Decimal: " + getBinary(binNumber));
+		}
 
 	}
 
-	static int[] stringToBinary(String str) {
+	static long getBinary(String str) {
 		char ch;
 		int[] array = new int[str.length()];
 
-		for (int i = 0; i < str.length(); i++) {
-			ch = str.charAt(i);
+		for (int i = str.length() - 1; i >= 0; i--) {
+			ch = str.charAt(str.length() - 1 - i);
 			if (ch == '1') {
 				array[i] = 1;
 			} else if (ch == '0') {
 				array[i] = 0;
 			} else {
-				array = new int[0];
-				return array;
+				return 0;
 			}
 
 		}
-		
-		return array;
+
+		return binToDecimal(array);
 	}
 
-	static int binToDecimal(int[] arr) {
-		int result = 0;
+	static long binToDecimal(int[] arr) {
+		long result = 0;
 		for (int i = 0; i < arr.length; i++) {
-			result += Math.pow(2, arr.length-i-1)*arr[i];
+
+			result += Math.pow(2, i) * arr[i];
 		}
+
 		return result;
-		
+
 	}
 
 }
