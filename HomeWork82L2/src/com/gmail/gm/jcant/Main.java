06c434ab8999c27f46cf4a754716c804094556bd
jcant <jcant.gm@gmail.com>
2018-01-22 16:16:29 +0200

    HW82L2 - count letters in text and rate they

---
+++ HomeWork82L2/src/com/gmail/gm/jcant/Main.java
@@ -0,0 +1,15 @@
+package com.gmail.gm.jcant;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String sss = "Some string of text";
+
+		for (int i = 0; i < sss.length(); i++) {
+			System.out.println(sss.codePointAt(i));
+		}
+
+	}
+
+}
